18fa4f6b4db0454ba3f409dceb3dcbdbb248ed22
lcastl <lcastellanosa@hotmail,com>
2022-01-19 01:46:09 -0500

    [Add] new login destination

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        binding.loginButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            )
+        }
+
+        binding.newLoginButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            )
+        }
+
+        return binding.root
+    }
+}
