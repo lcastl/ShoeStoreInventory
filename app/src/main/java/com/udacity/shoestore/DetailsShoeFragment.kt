0ec50090289b61319400e11b38e9f8fafc712bd4
lcastl <lcastellanosa@hotmail,com>
2022-01-25 02:21:33 -0500

    [Add] shoe details feture

--- app/src/main/java/com/udacity/shoestore/DetailsShoeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/DetailsShoeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentDetailsShoeBinding
 
 class DetailsShoeFragment : Fragment() {
@@ -14,8 +16,20 @@ class DetailsShoeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val binding: FragmentDetailsShoeBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_details_shoe, container, false
+        )
+
+        binding.saveButton.setOnClickListener {vie: View ->
+            vie.findNavController().navigate(
+                DetailsShoeFragmentDirections.actionDetailsShoeFragmentToShoeListFragment())
+
+        }
+
+        binding.cancelButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                DetailsShoeFragmentDirections.actionDetailsShoeFragmentToShoeListFragment())
         )
         return binding.root
     }
